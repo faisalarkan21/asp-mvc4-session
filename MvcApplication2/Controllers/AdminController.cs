d72c4d1a05cd75254dd4a4724c8c39f739b56d50
faisal arkan <faisalarkan21@gmail.com>
2017-10-20 00:18:40 +0700

    rebuild

--- MvcApplication2/Controllers/AdminController.cs
+++ MvcApplication2/Controllers/AdminController.cs
@@ -10,23 +10,23 @@ using System.Web.Mvc;
 
 namespace airlines_clone.Controllers
 {
-     [AuthorizationFilterAdmin]
+    [AuthorizationFilterAdmin]
     public class AdminController : Controller
     {
 
         static tiket_reservationEntities db = new tiket_reservationEntities();
 
-		// Bug 3 Fix
-		public void RefreshAllTable()
-		{
-			foreach (var entity in db.ChangeTracker.Entries())
-			{
-				entity.Reload();
-			}
-		}
+        // Bug 3 Fix
+        public void RefreshAllTable()
+        {
+            foreach (var entity in db.ChangeTracker.Entries())
+            {
+                entity.Reload();
+            }
+        }
 
-		// GET: Admin
-		public ActionResult dashboard_admin()
+        // GET: Admin
+        public ActionResult dashboard_admin()
         {
 
             Statistik statistik = new Statistik();
@@ -42,15 +42,15 @@ namespace airlines_clone.Controllers
             var checkPembeli = db.detil_pesan_tiket;
 
             // cek pembeli ada atau engga 
-            if (checkPembeli.Count() == 0 )
+            if (checkPembeli.Count() == 0)
             {
                 // biarkan kosong.
 
             }
             else
             {
-				// Bug # 1 Fix 
-				statistik.uang_estimasi = ConvertCurrency.
+                // Bug # 1 Fix 
+                statistik.uang_estimasi = ConvertCurrency.
                 ToRupiah(db.detil_pesan_tiket.Select(u
                 => u.harga_tiket).Sum());
 
@@ -81,9 +81,168 @@ namespace airlines_clone.Controllers
             return RedirectToAction("index", "Home");
         }
 
-       
 
 
+
+        public ActionResult semua_pembeli()
+        {
+            // Bug 3 Fix
+            RefreshAllTable();
+
+            var joinData = from p in db.pembeli
+                           from d in db.detil_pesan_tiket
+                           where p.id_pembeli == d.id_pembeli
+                           from v in db.pembeli_validasi
+                           where d.id_pembeli == v.id_pembeli
+                           select new Gabungan { tblPembeli = p, tblDetailTiket = d, tblValidasi = v };
+
+
+            return View(joinData);
+        }
+
+        public ActionResult user_detail(int id)
+        {
+            // Bug 3 Fix
+            RefreshAllTable();
+
+            Gabungan gabungan = new Gabungan();
+
+            gabungan.tblPembeli = db.pembeli.Find(id);
+            gabungan.tblDetailTiket = db.detil_pesan_tiket.Find(id);
+            gabungan.tblValidasi = db.pembeli_validasi.Find(id);
+
+            int pajak_berangkatId =
+            gabungan.tblDetailTiket.bandara_berangkat;
+            int pajak_tujuanId =
+            gabungan.tblDetailTiket.bandara_tujuan;
+
+            var hargaBerangkat =
+             db.pajak_bandara.Find(pajak_berangkatId);
+            var hargaTujuan = db.pajak_bandara.Find(pajak_tujuanId);
+
+            gabungan.rp_bandara_berangkat = ConvertCurrency.
+            ToRupiah(hargaBerangkat.pajak);
+            gabungan.rp_bandara_tujuan = ConvertCurrency.
+            ToRupiah(hargaTujuan.pajak);
+
+            gabungan.rp_harga_tiket = ConvertCurrency.
+            ToRupiah(gabungan.tblDetailTiket.harga_tiket);
+
+            gabungan.rp_total_transfer = ConvertCurrency.
+            ToRupiah(gabungan.tblDetailTiket.total_transfer);
+
+            gabungan.nm_bandara_berangkat =
+            hargaBerangkat.nm_bandara;
+            gabungan.nm_bandara_tujuan = hargaTujuan.nm_bandara;
+
+            return View(gabungan);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult user_detail(int id, Gabungan gabungan)
+        {
+
+            var user = db.pembeli.FirstOrDefault(u
+            => u.id_pembeli == id);
+            user.nm_pembeli = gabungan.tblPembeli.nm_pembeli;
+            user.email_pembeli = gabungan.tblPembeli.email_pembeli;
+            user.hp_pembeli = gabungan.tblPembeli.hp_pembeli;
+            user.password = gabungan.tblPembeli.password;
+            db.SaveChanges();
+
+            decimal UnformatRpTotalTf = ConvertCurrency
+            .ToAngka(gabungan.rp_total_transfer);
+            decimal TotalTf =
+            gabungan.tblDetailTiket.total_transfer;
+
+            if (UnformatRpTotalTf == TotalTf)
+            {
+                var userDetail =
+                db.detil_pesan_tiket.FirstOrDefault(u
+                                => u.id_pembeli == id);
+                userDetail.total_transfer = 0;
+
+                userDetail.status = 0;
+            }
+            else
+            {
+                var userDetail =
+                db.detil_pesan_tiket.FirstOrDefault(u
+                              => u.id_pembeli == id);
+                userDetail.total_transfer = UnformatRpTotalTf;
+
+                userDetail.status = 1;
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("semua_pembeli", "Admin");
+        }
+
+        [HttpPost]
+        public ActionResult delete_pembeli(int id, Gabungan gabungan)
+        {
+            var getUser = db.pembeli.SingleOrDefault(u
+            => u.id_pembeli == id);
+
+
+            if (getUser != null)
+            {
+                db.pembeli.Remove(getUser);
+                db.SaveChanges();
+            }
+            return RedirectToAction("semua_pembeli", "Admin");
+
+        }
+
+        public ActionResult pembeli_lunas()
+        {
+            // Bug 3 Fix
+            RefreshAllTable();
+
+            var joinData = from p in db.pembeli
+                           from d in db.detil_pesan_tiket
+                           where p.id_pembeli == d.id_pembeli
+                           from v in db.pembeli_validasi
+                           where d.id_pembeli == v.id_pembeli
+                           where d.total_transfer != 0
+                           select new Gabungan { tblPembeli = p, tblDetailTiket = d, tblValidasi = v };
+
+            return View(joinData);
+        }
+
+        public ActionResult pembeli_belum_lunas()
+        {
+            // Bug 3 Fix
+            RefreshAllTable();
+
+            var joinData = from p in db.pembeli
+                           from d in db.detil_pesan_tiket
+                           where p.id_pembeli == d.id_pembeli
+                           from v in db.pembeli_validasi
+                           where d.id_pembeli == v.id_pembeli
+                           where d.total_transfer == 0
+                           select new Gabungan { tblPembeli = p, tblDetailTiket = d, tblValidasi = v };
+
+            return View(joinData);
+        }
+
+        public ActionResult kotak_validasi()
+        {
+            // Bug 3 Fix
+            RefreshAllTable();
+
+            var joinData = from p in db.pembeli
+                           from d in db.detil_pesan_tiket
+                           where p.id_pembeli == d.id_pembeli
+                           from v in db.pembeli_validasi
+                           where d.id_pembeli == v.id_pembeli
+                           where v.pilihan_bank != null
+                           select new Gabungan { tblPembeli = p, tblDetailTiket = d, tblValidasi = v };
+
+            return View(joinData);
+        }
 
 
     }
--- MvcApplication2/Controllers/HomeController.cs
+++ MvcApplication2/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using airlines_clone.Helper;
+﻿using airlines_clone.helper;
+using airlines_clone.Helper;
+using airlines_clone.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +22,7 @@ namespace MvcApplication2.Controllers
         }
 
         public ActionResult login_user()
-        {            
+        {
             if (Session["user"] != null)
             {
                 return RedirectToAction("dashboard", "User");
@@ -38,6 +40,111 @@ namespace MvcApplication2.Controllers
             }
 
             return View();
+        }
+
+
+        public ActionResult daftar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult HargaBandara(int id)
+        {
+            var dataPajak = db.pajak_bandara.SingleOrDefault(u => u.id_bandara == id);
+            string harga = ConvertCurrency.ToRupiah(dataPajak.pajak);
+
+
+            return Json(new { harga = harga }, JsonRequestBehavior.AllowGet);
+        }
+        public static IEnumerable<SelectListItem> getBandara()
+        {
+            IEnumerable<SelectListItem> items = db.pajak_bandara.ToList().Select(c => new SelectListItem
+            {
+                Value = c.id_bandara.ToString(),
+                Text = c.nm_bandara
+            });
+            return items;
+        }
+
+        [HttpPost]
+        public ActionResult daftar(Gabungan gabungan)
+        {
+
+
+
+            if (gabungan.tblPembeli.password != gabungan.password_conf)
+            {
+
+
+                ViewBag.passTidakSama = "has-error";
+                ViewBag.errorMessage = "Password Konfirmasi Tidak Sama.";
+                return View();
+
+
+            }
+
+
+            string hashPass =
+              PBKDF2Encription.HashPassword
+                (gabungan.tblPembeli.password);
+
+
+
+            // table Pembeli
+            var dbPembeli = new pembeli
+            {
+                nm_pembeli = gabungan.tblPembeli.nm_pembeli,
+                email_pembeli = gabungan.tblPembeli.email_pembeli,
+                password = hashPass,
+                hp_pembeli = gabungan.tblPembeli.hp_pembeli,
+                gd_pembeli = gabungan.tblPembeli.gd_pembeli
+            };
+
+            db.pembeli.Add(dbPembeli);
+            db.SaveChanges();
+
+            //table tgl Order
+            tgl_pesan tgl_table = new tgl_pesan();
+            tgl_table.tgl_order =
+            DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            var dbTglPesan = new tgl_pesan
+            {
+                nm_pembeli = gabungan.tblPembeli.nm_pembeli,
+                tgl_order = tgl_table.tgl_order
+            };
+
+            db.tgl_pesan.Add(dbTglPesan);
+            db.SaveChanges();
+            // table Detail Pembeli
+            var dbPembeliDetail = new detil_pesan_tiket
+            {
+                nm_pembeli = gabungan.tblPembeli.nm_pembeli,
+                harga_tiket =
+            ConvertCurrency.ToAngka(gabungan.rp_harga_tiket),
+                total_transfer = gabungan.tblDetailTiket.total_transfer,
+                pilihan_bank = gabungan.tblDetailTiket.pilihan_bank,
+                bandara_berangkat = gabungan.tblDetailTiket.bandara_berangkat,
+                bandara_tujuan = gabungan.tblDetailTiket.bandara_tujuan,
+                status = gabungan.tblDetailTiket.status
+            };
+
+            db.detil_pesan_tiket.Add(dbPembeliDetail);
+            db.SaveChanges();
+            // table Validasi Pembeli
+            var dbValidasi = new pembeli_validasi
+            {
+                nm_pembeli = gabungan.tblPembeli.nm_pembeli,
+                email_pembeli = gabungan.tblPembeli.email_pembeli,
+                hp_pembeli = gabungan.tblPembeli.hp_pembeli,
+                uang_transfer_validasi = null,
+                pilihan_bank = null
+            };
+            db.pembeli_validasi.Add(dbValidasi);
+            db.SaveChanges();
+            return RedirectToAction("login_user", "Home");
+
         }
 
         public void RefreshAllTable()
